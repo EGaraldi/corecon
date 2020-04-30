0351874179186f6b1e4833bee7d000b6e8711b14
Enrico Garaldi <egaraldi@mpa-garching.mpg.de>
2020-04-30 12:21:45 +0200

    brought in pypi files

---
+++ README.md
@@ -0,0 +1,15 @@
+CoReCon: open collection of Reionization Constraints
+====================================================
+
+**CoReCon** is an open collection of constraints on various physical
+quantities linked to the *Epoch of Reionization (EoR)*.
+
+It is built to be easily complemented by contribution from the scientific
+community, thanks to a simple data form that supports two different ways of
+input data.
+
+CoReCon takes care of loading and interpreting the data, and presenting them
+in an organic and ready-to-use way. It also implement simple slicing capabilities,
+which allow to perform simple data filtering.
+
+See https://corecon.readthedocs.io/en/latest/ for the latest documentation.
---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'corecon',
+  packages = find_packages(exclude=["docs"]),
+  version = '0.2.2',
+  license='gpl-3.0',
+  description = 'an open collection of constraints on the Epoch of Reionization',
+  long_description=long_description,
+  author = 'Enrico Garaldi',
+  author_email = 'enrico.garaldi@gmail.com', 
+  url = 'https://github.com/EGaraldi/corecon',
+  download_url = 'https://github.com/EGaraldi/corecon/archive/v0.2.2.tar.gz',
+  keywords = ['constraints', 'reionization', 'cosmic', 'observations', 'data'],
+  install_requires=['numpy'],
+  classifiers=[
+    'Development Status :: 4 - Beta', 
+    'Intended Audience :: Science/Research',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
