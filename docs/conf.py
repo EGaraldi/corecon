2a8b750837c47b5ef3efd17f2e0c39dea2fb4900
Enrico Garaldi <egaraldi@mpa-garching.mpg.de>
2020-04-30 10:51:43 +0200

    attempt of rtd yml file

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Enrico Garaldi'
 author = 'Enrico Garaldi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
