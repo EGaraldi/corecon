b7a6938853431ca60a35b6e55ba1f498369ab53f
Enrico Garaldi <egaraldi@mpa-garching.mpg.de>
2020-06-28 15:48:48 +0200

    Updated version number to 0.2.7

--- corecon/__init__.py
+++ corecon/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Enrico Garaldi"
 
 __license__ = "GPLv3"
 
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 
 __description__ ="""
 CoReCon
--- docs/index.rst
+++ docs/index.rst
@@ -1,4 +1,4 @@
-.. image:: ../logo.png
+.. image:: ../logo_gray.png
 
 .. _CoReCon:
 
