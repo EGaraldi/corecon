bb8a3ae0eb00c359dd5e778a84fc68fca799ea52
Enrico Garaldi <egaraldi@mpa-garching.mpg.de>
2022-07-18 02:25:14 +0200

    removed DjangoApp

--- DjangoApp/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 manage.py runserver & xdg-open http://localhost:8000/admin/ & xdg-open http://localhost:8000/constraints/ion_frac  & xdg-open http://localhost:8000/search/ion_frac
-
-
