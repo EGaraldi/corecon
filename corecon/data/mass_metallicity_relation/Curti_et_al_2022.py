a920cdb5ea570764c8f6ec98672231506e335126
Enrico Garaldi <egaraldi@mpa-garching.mpg.de>
2022-07-27 11:54:08 +0200

    added Curti et al. 2022

---
+++ corecon/data/mass_metallicity_relation/Curti_et_al_2022.py
@@ -0,0 +1,30 @@
+dictionary_tag         = "Curti et al. 2022"
+
+reference              = "Curti, D'Eugenio, Carniani, Maiolino, Sandels, Witstok, et al.; subm. (2022)"
+
+url                    = "https://arxiv.org/pdf/2207.12375"
+
+description            = \
+"""
+From the properties of 3 galaxies at z~8 behind the galaxy cluster SMACS J0723.3-7327 observed by JWST using the NIRSpec instrument.
+"""
+
+data_structure         = "points" #grid or points
+
+extracted              = False
+
+ndim                   = 2
+
+dimensions_descriptors = ["redshift", "log stellar mass [Msun]"]
+
+axes                   = [[8.496, 7.75], [7.665, 8.72], [7.658, 8.08]]
+
+values                 = [7.7486, 8.0787, 8.7171]
+
+err_up                 = [0.1133, 0.1224, 0.0710]
+
+err_down               = [0.1193, 0.1163, 0.0801]
+
+upper_lim              = False
+
+lower_lim              = False
