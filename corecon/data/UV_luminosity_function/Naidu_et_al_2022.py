aec1ad0585ac5b02049529b0c7af29fa55680972
Enrico Garaldi <egaraldi@mpa-garching.mpg.de>
2022-07-27 10:24:23 +0200

    Added Naidu et al. UVLF point

---
+++ corecon/data/UV_luminosity_function/Naidu_et_al_2022.py
@@ -0,0 +1,32 @@
+dictionary_tag         = "Naidu et al. 2022"
+
+reference              = "Naidu, Oesch, van Dokkum, Nelson, Suess, Whitaker, Allen, et al.; arXiv (2022)"
+
+url                    = "https://arxiv.org/pdf/2207.09434.pdf"
+
+description            = \
+"""
+Based on a search for luminous z > 10 galaxies with JWST /NIRCam photometry spanning
+≈ 1 − 5μm and covering 49 arcmin2 from the public JWST Early Release Science programs (CEERS
+and GLASS). This resulted in 2 candidates  with M_UV ~ -21 and redshiftf z~11 and z~13.
+"""
+
+data_structure         = "points" #grid or points
+
+extracted              = False
+
+ndim                   = 2
+
+dimensions_descriptors = ["redshift", "M_UV"]
+
+axes                   = [[11.5, -21.0]]
+
+values                 = [-5.05]
+
+err_up                 = [0.37]
+
+err_down               = [0.45]
+
+upper_lim              = False
+
+lower_lim              = False
