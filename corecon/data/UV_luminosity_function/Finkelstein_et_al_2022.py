8dcc8415d82eaf3de28f1a3a19d5b879b5e24563
Enrico Garaldi <egaraldi@mpa-garching.mpg.de>
2022-08-02 08:57:51 +0200

    added Finkelstein_et_al_2022

---
+++ corecon/data/UV_luminosity_function/Finkelstein_et_al_2022.py
@@ -0,0 +1,34 @@
+dictionary_tag         = "Finkelstein et al. 2022"
+
+reference              = "Finkelstein, Bagley, Haro, Dickinson, Ferguson, Kartaltepe, Papovich, et al.; arXiv (2022)"
+
+url                    = "https://arxiv.org/pdf/2207.12474.pdf"
+
+description            = \
+"""
+Based on a single galaxy candidate in the JWST's CEERS survey with photometric redshift z=14.3 detected in 5 filters at >5sigma significance. 
+"""
+
+data_structure         = "points" #grid or points
+
+extracted              = False
+
+ndim                   = 2
+
+dimensions_descriptors = ["redshift", "M_UV"]
+
+axes                   = [[14.3, -20.3]]
+
+values                 = [-5.1249]
+
+err_up                 = [0.3554]
+
+err_down               = [0.0827]
+
+err_right              = [0.1]
+
+err_left               = [0.1]
+
+upper_lim              = False
+
+lower_lim              = False
