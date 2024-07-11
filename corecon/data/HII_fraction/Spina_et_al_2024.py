6ac34d1a9bb7aea5fd2987285201b24537f1665a
Enrico Garaldi <egaraldi@mpa-garching.mpg.de>
2024-07-11 12:10:44 +0200

    add HII constraints

---
+++ corecon/data/HII_fraction/Spina_et_al_2024.py
@@ -0,0 +1,39 @@
+dictionary_tag = "Spina et al. 2024"
+
+reference   = "Spina, Bosman, Davies, Gaikwad, Zhu; subm. (2024)"
+ 
+url         = "https://ui.adsabs.harvard.edu/abs/2024arXiv240512273S/abstract"
+
+description = \
+"""Measured from the damping wing detected comparing long and short dark gaps in the Lyman-alpha forest 
+(assuming long gaps come from neutral island and short from fully-ionised medium) in 38 X-shooter spectra 
+at 5.4 < z < 6.5 with S/N>20 per 10 km/s pixel. 
+"""
+
+data_structure         = "points" #grid or points
+
+extracted              = False
+
+ndim                   = 1
+
+dimensions_descriptors = ["redshift"]
+
+axes                   = [5.67, 5.89]
+
+err_left               = [0.27, 0.13]
+
+err_right              = [0.09, 0.21]
+
+values      = [0.81, 0.56]
+
+err_down    = [0.07, None]
+
+err_up      = [0.07, None]
+
+err_down2   = [0.11, None]
+
+err_up2     = [0.06, None]
+
+upper_lim     = False
+
+lower_lim     = [False, True]
---
+++ corecon/data/HII_fraction/Zhu_et_al_2024.py
@@ -0,0 +1,30 @@
+dictionary_tag = "Zhu et al. 2024"
+
+reference   = "Zhu, Becker, Bosman, Cain, Keating, et al; subm. (2024)"
+ 
+url         = "https://ui.adsabs.harvard.edu/abs/2024MNRAS.tmpL..59Z/abstract"
+
+description = \
+"""Measured from the damping wing detected in the Lyman-alpha forest around long gaps in the Lyman-beta forest (assuming the latter track
+neutral islands) in 42 spectra at 5.77 < z < 6.31. 
+"""
+
+data_structure         = "points" #grid or points
+
+extracted              = False
+
+ndim                   = 1
+
+dimensions_descriptors = ["redshift"]
+
+axes                   = [5.8]
+
+values      = [0.939]
+
+err_down    = [0.039]
+
+err_up      = [0.039]
+
+upper_lim     = True
+
+lower_lim     = False
